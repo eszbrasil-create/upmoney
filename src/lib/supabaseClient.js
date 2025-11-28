5aecc3d4b9ae687bf3002c0ca7651f2164bde7de
Emerson Alves de Souza <emersonalvesdesouza@Mac.Home>
2025-11-28 20:49:20 +0000

    sua mensagem

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,8 @@
+// src/lib/supabaseClient.js
+
 import { createClient } from "@supabase/supabase-js";
 
-// Cole aqui:
-const SUPABASE_URL = "SUA_URL_AQUI";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://pbcqsapoxcioqhqonstf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Z7NSZSRMW0sWsqdfGyXyiw_UZHGtcZt";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
